a83efb72df2adc7d0a3c3bfd54937f517202358b
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-09-11 11:28:58 -0600

    pushh

--- autocomplete.js
+++ autocomplete.js
@@ -4,17 +4,7 @@ const createAutoComplete = ({
   onOptionSelect,
   inputValue,
   fetchData
-}) => {
-  root.innerHTML = `
-    <label><b>Search</b></label>
-    <input class="input" />
-    <div class="dropdown">
-      <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-      </div>
-    </div>
-  `;
-
+}) 
   const input = root.querySelector('input');
   const dropdown = root.querySelector('.dropdown');
   const resultsWrapper = root.querySelector('.results');
